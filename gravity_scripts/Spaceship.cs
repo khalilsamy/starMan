816d9d8a882c2f8ab93f5442e48638c6e58a772c
Samy Khalil <samy.khalilgm@gmail.com>
2018-07-22 23:53:29 +0200

    change attraction behavior

--- gravity_scripts/Spaceship.cs
+++ gravity_scripts/Spaceship.cs
@@ -4,53 +4,54 @@ using UnityEngine;
 
 public class Spaceship : MonoBehaviour {
 
-    public float speed;
-    public GameObject attractedPlanet;
+    private bool rotating;
 
+    public float gap; // 
+    public float speed; // 
+    
+    // Planet which can be Attract this spaceShip
+    public GameObject attractedPlanet;
+    
+    // booolean value to know if this spaceship is attracted
     public bool attracted;
 
-    public Vector3 desiredPosition;
-
-    private Vector3 rotationMask_f;
-    public float gap;
+    // Vector3 of next position
+    public Vector3 desiredPosition; 
 
     // Use this for initialization
     void Start () {
-        attracted = false;
-        rotationMask_f = Vector3.back; //new Vector3(1, 1, 0); 
-        gap = 2f;
+        //any attraction effective at start
+        attracted = false; 
+        // Go straigt in one direction for this exemple
+        // rotationMask_f = Vector3.back;
+        // try to set a gap to increase or decrease position of 
+        // spaceship on orbit
+        gap = 0; 
     }
 	
 	// Update is called once per frame
 	void Update () {
-      
+
         if (attracted)
         {
             float distance = Vector3.Distance(attractedPlanet.transform.position,
                                               transform.position);
-           /* if (distance < gap)
-            {*/
-                desiredPosition = (transform.position - attractedPlanet.transform.position).normalized * gap + attractedPlanet.transform.position;
-                transform.position = Vector3.MoveTowards(transform.position, desiredPosition, Time.deltaTime * speed);
-                Vector3 tangentVector = Quaternion.Euler(0, 90, 0) * (transform.position - attractedPlanet.transform.position);
-                transform.rotation = Quaternion.LookRotation(tangentVector);
-            /*rotationMask_f = new Vector3(0, transform.position.y * 0.025f, 
-                                                transform.position.z * 0.025f);*/
-            /*}
-            else
-            {
-               rotationMask_f = Vector3.back;
-            }
-            transform.RotateAround(attractedPlanet.transform.position, rotationMask_f,
-               100.0f * Time.deltaTime);*/
+       
+            // orient spaceship move to the planet 
+            transform.LookAt(attractedPlanet.transform, Vector3.back);
+            Vector3 gravitation = new Vector3(1,0,gap);
+            transform.Translate(gravitation * Time.deltaTime);
+        
         }
+
         else
         {
-          
-                // Move control normal
-                transform.position += Vector3.right * speed * Time.deltaTime;
-            
+
+            // Move control normal
+            //transform.position += Vector3.right * speed * Time.deltaTime;
+            transform.Translate(Vector3.right * Time.deltaTime);
         }
+        
     }
-    
+
 }
